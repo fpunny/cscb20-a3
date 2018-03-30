57e5eeb5e534b59bbbda18418c08036885fdf5b1
Nanciiii <37058271+Nanciiii@users.noreply.github.com>
2018-03-30 15:28:14 -0400

    new one

--- includes/views/Login.php
+++ includes/views/Login.php
@@ -4,13 +4,13 @@
     <div align = "center">
       <div class = "margin" align = "left">
         <div class = "topback"><b>Login</b>
-          <form action = "">
-            <input type = 'radio' name = "student" value = "Student">Student<br>
-            <input type = 'radio' name="Ta" value = "Ta">TA<br>
-            <input type = 'radio' name="professor" value = "Professor">professor<br>
-          </form>
         </div>
           <div class = "innermargin">
+            <form action = "">
+              <input type = 'radio' name = "student" value = "Student">Student<br>
+              <input type = 'radio' name="Ta" value = "Ta">TA<br>
+              <input type = 'radio' name="professor" value = "Professor">professor<br>
+            </form>
             <form action = "" method = "post">
               <label> UserName   :</label><input type = "text" name = "username" class = 'box'/><br /><br />
               <label> Password   :</label><input type = "password" name = "password" class = "box"  /><br /><br />
@@ -18,5 +18,5 @@
             </form>
           <div id="space"><?php echo $error; ?></div>
       </div>
-    </div> 
+    </div>
 </body>
