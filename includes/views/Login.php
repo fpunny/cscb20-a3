7809bc46b8d29d8f156b85a4eec38ac965541ea7
Nanciiii <37058271+Nanciiii@users.noreply.github.com>
2018-03-30 19:45:04 -0400

    new php and new css

--- includes/views/Login.php
+++ includes/views/Login.php
@@ -1,17 +1,19 @@
-  <div align = "center">
-    <div class = "margin" align = "left">
-      <div class = "topback"><b>Login</b>
-      </div>
-        <div class = "innermargin">
-          <form action = "">
-            <input type = 'radio' name = "student" value = "Student">Student
-            <input type = 'radio' name="Ta" value = "Ta">TA
-            <input type = 'radio' name="professor" value = "Professor"> Professor
-          </form>
-              <form action = "" method = "post">
-                <input type = "text" placeholder = "UserName"> <br>
-                <input type = "password" placeholder = "Password"><br>
-                <input type = "submit" value = "Login"/><br />
+<div align = "center">
+  <div class = "margin" align = "left">
+    <div class = "topback"><b>SIGNUP</b>
+    </div>
+      <div class = "innermargin" >
+        <form action = "">
+          <input type = 'radio' name = "student" value = "Student">Student
+          <input type = 'radio' name="Ta" value = "Ta">TA
+          <input type = 'radio' name="professor" value = "Professor"> Professor
+        </form>
+            <form action = "" method = "post">
+              <input type = "text" class = "adjust" placeholder = "UserName"> <br>
+              <input type = "password" class = "adjust" placeholder = "Password"><br>
+            </div>
+                <div class ="center">
+                <input type = "submit" class ="button" align = 'center' value = "Login"><br />
+              </div>
             </form>
-          </div>
-  </div>
+        </div>
